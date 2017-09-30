0802f5bc27918982efeeab7ad65d4bd26d21f051
Thomas Sanjurjo <sanjurjo7@gmail.com>
2017-09-30 07:40:02 -0400

    TODO: Fix movement keys, currently commanded by dx and dy

--- app.py
+++ app.py
@@ -6,38 +6,36 @@ screen = pygame.display.set_mode((1024,768))
 clock = pygame.time.Clock()
 
 class CharSprite(pygame.sprite.Sprite):
-    GRAVITY = (0,2)
-    MAX_FORWARD_SPEED = (10,0)
-    MAX_DOWN_SPEED = (0,100)
-    RUN_SPEED = (2,0)
-    JUMP_FORCE = (0,-14)
+    GRAVITY = 2
+    MAX_DOWN_SPEED = 100
+    RUN_SPEED = 2
+    JUMP_FORCE = -14
     spritesheet = spritesheet.Spritesheet('JonesSheet.png')
 
     def __init__(self, image, position):
         pygame.sprite.Sprite.__init__(self)
         self.src_image = self.spritesheet.image_at((0,0,32,64))
         self.position = position
-        self.speed = (0,0)
         self.direction = 'right'
         self.image = self.src_image
         self.rect = self.src_image.get_rect()
-        self.jump = False
+        self.jump_t = False
         self.rect.center = self.position
+        self.dx, self.dy = 0,0
+        self.speed = (0,0)
 
     def update(self, deltat):
         # SIMULATION
-        if self.position[1] < 480:
-            self.speed = tuple(map(sum,zip(self.speed,self.GRAVITY)))
-            if self.speed[1] > self.MAX_DOWN_SPEED[1]:
-                self.speed[1] = self.MAX_DOWN_SPEED[1]
-        elif self.jump and self.speed[1] == 0:
-            self.speed = tuple(map(sum,zip(self.speed,self.JUMP_FORCE)))
+        if self.position[1] < 580:
+            self.dy += self.GRAVITY
+            if self.dy > self.MAX_DOWN_SPEED:
+                self.dy = self.MAX_DOWN_SPEED
         else:
-            self.speed = (0,0)
+            self.dy = 0
+        self.speed = (self.dx,self.dy)
         x, y = self.position
         self.position = tuple(map(sum,zip((x,y),self.speed)))
         if self.speed == (0,0):
-            self.jump = False
             self.src_image = self.spritesheet.image_at((0,0,32,64))
         elif self.speed != 0:
             self.src_image = self.spritesheet.image_at((320,0,32,64))
@@ -47,11 +45,15 @@ class CharSprite(pygame.sprite.Sprite):
         self.rect.center = self.position
 
     def run(self, direction):
-        if self.position[0] < 600:
-            self.speed = tuple(map(sum,zip(self.speed,self.RUN_SPEED)))
+        if self.position[0] < 1200:
+            self.dx += self.RUN_SPEED
+        run_cycle = [
+            (480,0,32,64), (512,0,32,64), (544,0,32,64),
+            (576,0,32,64), (608,0,32,64), (640,0,32,64)]
 
     def jump(self, direction):
-        pass
+        if not self.jump_t and self.dy == 0:
+            self.dy += self.JUMP_FORCE
 
 # CREATE CHARACTER AND RUN
 rect = screen.get_rect()
@@ -64,9 +66,8 @@ while 1:
         if not hasattr(event, 'key'): continue
         down = event.type == KEYDOWN
         if event.type == KEYDOWN:
-            if event.key == K_w: character.jump = True
+            if event.key == K_w: character.jump('right')
             if event.key == K_d: character.run('right')
-            if event.key == K_a: character.run('left')
         if event.key == K_ESCAPE: sys.exit(0)
 
     # RENDERING
