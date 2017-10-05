a0346b2ea1c6b18d5a56631bbe759a0e6168fa89
Thomas Sanjurjo <sanjurjo7@gmail.com>
2017-10-05 05:58:28 -0400

    Organized image handling better

--- app.py
+++ app.py
@@ -4,21 +4,20 @@ from pygame.locals import *
 
 screen = pygame.display.set_mode((1024,768))
 clock = pygame.time.Clock()
-transp = (255,0,255)
 
 class CharSprite(pygame.sprite.Sprite):
     GRAVITY = 2
     MAX_DOWN_SPEED = 100
     RUN_SPEED = 10
-    JUMP_FORCE = -14
+    JUMP_FORCE = -20
     spritesheetJ = spritesheet.Spritesheet('JonesSheet.png')
 
     def __init__(self, image, position):
         pygame.sprite.Sprite.__init__(self)
-        self.image = self.spritesheetJ.image_at((128,0,32,64))
+        self.image = self.spritesheetJ.image_at((128,0,32,64),colorkey=-1)
+        self.rect = self.image.get_rect()
         self.position = position
         self.direction = 'right'
-        self.rect = self.image.get_rect()
         self.rect.center = self.position
         self.dx, self.dy = 0,0
         self.speed = (0,0)
@@ -31,19 +30,19 @@ class CharSprite(pygame.sprite.Sprite):
         # FIXME: This is a placeholder for collision
         if self.dy >= 0 and self.position[1] >= 580:
             if self.dy > 0 or self.dy < -14:
-                self.idle('right')
+                self.idle()
         # Gravity as a constant falling force
         if self.fall:
             self.dy += self.GRAVITY
             if self.dy > self.MAX_DOWN_SPEED:
                 self.dy = self.MAX_DOWN_SPEED
-            self.anim_cycle('jump', 'right')
+            self.anim_cycle('jump')
 
         # Calculate speed vectors
         self.speed = (self.dx,self.dy)
         x, y = self.position
         self.position = tuple(map(sum,zip((x,y),self.speed)))
-       
+
         # Gather image and ready display
         # FIXME: Images display and refresh too quickly
         self.image = self.curr_anim[self.frame]
@@ -52,7 +51,7 @@ class CharSprite(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = self.position
 
-    def anim_cycle(self, name, direction):
+    def anim_cycle(self, name):
         animations = {
             'idle': [
                 (0,0,32,64),(32,0,32,64),
@@ -63,19 +62,19 @@ class CharSprite(pygame.sprite.Sprite):
             'run': [
                 (480, 0, 32, 64),(512,0,32,64),(544,0,32,64),
                 (576,0,32,64),(608,0,32,64),] }
-        # FIXME: this code isn't causing the images to flip?
         self.curr_anim = self.spritesheetJ.images_at(
-            animations[name], colorkey=transp)
-        if direction == 'left':
-            self.curr_anim = self.spritesheetJ.imagesR_at(animations[name], colorkey=transp)
+            animations[name], colorkey=-1)
+        if self.direction == 'left':
+            self.curr_anim = self.spritesheetJ.imagesR_at(
+                animations[name], colorkey=-1)
         self.frame = 0
 
-    def idle(self, direction):
+    def idle(self):
         if self.fall:
             self.fall = False
             self.dy = 0
         self.dx = 0
-        self.anim_cycle('idle', 'right')
+        self.anim_cycle('idle')
 
     def run(self, direction):
         # ANIMATION GATHER
@@ -83,10 +82,12 @@ class CharSprite(pygame.sprite.Sprite):
             for i in range(self.RUN_SPEED):
                 if direction == 'left':
                     self.dx -= 1
-                    self.anim_cycle('run', 'left')
+                    self.direction = 'left'
+                    self.anim_cycle('run')
                 else:
                     self.dx += 1
-                    self.anim_cycle('run', 'right')
+                    self.direction = 'right'
+                    self.anim_cycle('run')
 
     def jump(self, direction):
         if not self.fall:
@@ -99,7 +100,7 @@ character = CharSprite('idle', rect.center)
 p_group = pygame.sprite.RenderPlain(character)
 while 1:
     # USER INPUT
-    deltat = clock.tick(10)
+    deltat = clock.tick(30)
     for event in pygame.event.get():
         if not hasattr(event, 'key'): continue
         down = event.type == KEYDOWN
@@ -108,8 +109,8 @@ while 1:
             if event.key == K_d: character.run('right')
             if event.key == K_a: character.run('left')
         if event.type == KEYUP:
-            if event.key == K_a: character.idle('left')
-            if event.key == K_d: character.idle('right')
+            if event.key == K_a: character.idle()
+            if event.key == K_d: character.idle()
         if event.key == K_ESCAPE: sys.exit(0)
 
     # RENDERING
